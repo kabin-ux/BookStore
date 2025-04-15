e8e90277a52a35befa1e33c54f80faa34195efed
AnlessXtha <aadesh.shrrstha@gmail.com>
2025-04-15 21:48:33 +0545

    structure setup of project

---
+++ BookStore/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BookStore
+{
+    public class ApplicationDBContext : IdentityDbContext<Users, Roles, long>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }    
+    }
+}
---
+++ BookStore/Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Entities
+{
+
+    public class Users: IdentityUser<long>
+    {
+        [Key]
+        public int Id { get; set; }
+        public  string Name { get; set; }
+        public  string Email { get; set; }
+        public  string Password { get; set; }
+        public  string Username { get; set; }
+        public  string ContactNumber { get; set; }
+
+    }
+}
+
